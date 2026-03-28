e1251f810e049f1d6f84b4c1c4aa9abf4609a2d4
subhangi03 <subhangip2005@gmail.com>
2026-03-28 12:36:26 +0530

    Parallel Testing
    # Conflicts:
    #	Automation_Testing/selenium-ui-automation/test-output/SparkReport/Spark.html

--- Automation_Testing/selenium-ui-automation/src/test/java/com/freecrm/automation/runners/TestRunnerTasks.java
+++ Automation_Testing/selenium-ui-automation/src/test/java/com/freecrm/automation/runners/TestRunnerTasks.java
@@ -2,10 +2,17 @@ package com.freecrm.automation.runners;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
 
 @CucumberOptions(features = {"src/test/resources/featureFiles/tasks"},
-        glue = "com.freecrm.automation.stepDefinitions.tasks",
-        tags="@TC_Task_001",
+        glue = {"com.freecrm.automation.stepDefinitions.tasks","com.freecrm.automation.hooks"},
+        tags="@TC_Task",
+        plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
         dryRun = false)
 public class TestRunnerTasks extends AbstractTestNGCucumberTests {
+    @Override
+    @DataProvider(parallel = false)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
 }
